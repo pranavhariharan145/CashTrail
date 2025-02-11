3b5686c996576a162c72f7ad0960fcaad6487260
Pranav <pranavhariharan145@gmail.com>
2025-02-11 21:41:40 +0530

    add: a simple get request in routes

--- backend/routes/expenses.js
+++ backend/routes/expenses.js
@@ -3,7 +3,7 @@ const Expense = require("../models/expschema");
 
 const router = express.Router();
 
-// Add a new expense
+// Add a new expense POST
 router.post("/", async (req, res) => {
   try {
     const expense = new Expense(req.body);
@@ -13,5 +13,16 @@ router.post("/", async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 });
+
+// GET request to retreive the data in mongo as json    
+router.get("/", async (req, res) => {
+    try {
+      const expenses = await Expense.find(); // Fetch all expenses from MongoDB
+      res.json(expenses); // Send response
+    } catch (error) {
+      res.status(500).json({ error: "Failed to fetch expenses" });
+    }
+  });
+  
 
 module.exports = router;
